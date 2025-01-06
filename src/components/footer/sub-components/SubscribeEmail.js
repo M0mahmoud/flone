2dcbc7b247ab3dc0d9015e2c7de70fc4febed0a3
M0mahmoud <houdmohamed85@gmail.com>
2025-01-06 22:29:37 +0200

    Sticky

--- src/components/footer/sub-components/SubscribeEmail.js
+++ src/components/footer/sub-components/SubscribeEmail.js
@@ -1,59 +1,58 @@
 import PropTypes from "prop-types";
 import React, { useRef, useState } from "react";
-import MailchimpSubscribe from "react-mailchimp-subscribe";
 import { multilanguage } from "redux-multilanguage";
 import axiosInstance from "../../../api/api";
 
-const CustomForm = ({ status, message, onValidated }) => {
-  let email;
-  const submit = () => {
-    email &&
-      email.value.indexOf("@") > -1 &&
-      onValidated({
-        EMAIL: email.value,
-      });
+// const CustomForm = ({ status, message, onValidated }) => {
+//   let email;
+//   const submit = () => {
+//     email &&
+//       email.value.indexOf("@") > -1 &&
+//       onValidated({
+//         EMAIL: email.value,
+//       });
 
-    let emailInput = document.getElementById("mc-form-email");
-    emailInput.value = "";
-  };
+//     let emailInput = document.getElementById("mc-form-email");
+//     emailInput.value = "";
+//   };
 
-  return (
-    <div className="subscribe-form">
-      <div className="mc-form">
-        <div>
-          <input
-            id="mc-form-email"
-            className="email"
-            ref={(node) => (email = node)}
-            type="email"
-            placeholder="Enter your email address..."
-          />
-        </div>
-        <div className="clear">
-          <button className="button" onClick={submit}>
-            SUBSCRIBE
-          </button>
-        </div>
-      </div>
+//   return (
+//     <div className="subscribe-form">
+//       <div className="mc-form">
+//         <div>
+//           <input
+//             id="mc-form-email"
+//             className="email"
+//             ref={(node) => (email = node)}
+//             type="email"
+//             placeholder="Enter your email address..."
+//           />
+//         </div>
+//         <div className="clear">
+//           <button className="button" onClick={submit}>
+//             SUBSCRIBE
+//           </button>
+//         </div>
+//       </div>
 
-      {status === "sending" && (
-        <div style={{ color: "#3498db", fontSize: "12px" }}>sending...</div>
-      )}
-      {status === "error" && (
-        <div
-          style={{ color: "#e74c3c", fontSize: "12px" }}
-          dangerouslySetInnerHTML={{ __html: message }}
-        />
-      )}
-      {status === "success" && (
-        <div
-          style={{ color: "#2ecc71", fontSize: "12px" }}
-          dangerouslySetInnerHTML={{ __html: message }}
-        />
-      )}
-    </div>
-  );
-};
+//       {status === "sending" && (
+//         <div style={{ color: "#3498db", fontSize: "12px" }}>sending...</div>
+//       )}
+//       {status === "error" && (
+//         <div
+//           style={{ color: "#e74c3c", fontSize: "12px" }}
+//           dangerouslySetInnerHTML={{ __html: message }}
+//         />
+//       )}
+//       {status === "success" && (
+//         <div
+//           style={{ color: "#2ecc71", fontSize: "12px" }}
+//           dangerouslySetInnerHTML={{ __html: message }}
+//         />
+//       )}
+//     </div>
+//   );
+// };
 
 const SubscribeEmail = ({ strings }) => {
   const [email, setEmail] = useState("");
